17cc947eaf86c5cdea99b46398ad94db9fd61fc6
RadLea <radu.leanca@softwire.com>
2023-06-30 12:18:55 +0300

    added /rovers route

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import {Response, Request} from "express";
+import axios from 'axios';
 
 const express = require("express");
 const port = 8008;
@@ -8,6 +9,18 @@ const app = express();
 app.use(express.json());
 const router = express.Router();
 router.get('/test', (req: Request, res: Response) => res.send('Hello world !'));
+router.get('/rovers', (req: Request, res: Response) => {
+    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=0ph5CMFkcljUN0DfeSUKMSkDY4WsdNtFjAgMVJ0K')
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            console.log("Yay");
+        });
+});
 app.use('/', router);
 
 app.listen(port, () => {
